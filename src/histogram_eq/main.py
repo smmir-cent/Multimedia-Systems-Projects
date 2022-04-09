82126544bda6330cf2830b2555cb75cf51897976
smmir-cent <76453689+smmir-cent@users.noreply.github.com>
2022-04-09 17:46:04 +0000

    01/01/20 - 22:15 => complete and tested

--- src/histogram_eq/main.py
+++ src/histogram_eq/main.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 
 width, height = 0, 0
 histogram_amount = np.zeros(256)
+histogram_amount_result = np.zeros(256)
 cumulative_histogram = np.zeros(256)
 
 
@@ -30,6 +31,7 @@ def step_1(image):
     width, height = image.size
     grayscale_image = rgb2gray(image=image)
     grayscale_image.save("assets/grayscale.png")
+    return grayscale_image
 
 
 def step_2():
@@ -42,11 +44,27 @@ def step_3():
     global histogram_amount,cumulative_histogram
     cumulative_histogram = np.cumsum(histogram_amount)
 
+def step_4_5(grayscale_image):
+    global histogram_amount,cumulative_histogram,histogram_amount_result,width,height
+    result_image = Image.new(mode = 'L', size = (width, height)) 
+    for c in range(len(cumulative_histogram)):
+        histogram_amount_result[c] = int(round((255*(cumulative_histogram[c])/(width*height))))
+    
+    # print("-----------gray-----------")
+    for i in range(height):
+        for j in range(width):
+            pix_val = grayscale_image.getpixel((j,i))
+            result_image.putpixel((j, i),int(histogram_amount_result[pix_val]))
+    result_image.save("assets/final_result.png")
+
+
 if __name__ == '__main__':
     img_location = input("Enter your image : ")
     image = Image.open(img_location)
-    step_1(image)
+    grayscale_image = step_1(image)
     step_2()
+    step_3()
+    step_4_5(grayscale_image)
     
 
 ### assets/image.png
